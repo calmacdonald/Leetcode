ec6d86dfd5ad240376cdb14fefbc364abe3900f9
cmacdona <cmacdona@eecs.wsu.edu>
2024-12-09 20:36:49 -0800

    added problems

---
+++ C++/Distribute Candies Among Children.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int distributeCandies(int n, int limit) {
+        int ans = 0;
+        for (int i = 0; i <= limit; i++){
+            for (int j = 0; j <= limit; j++){
+                int x = n - i - j;
+                if ((x >= 0) && (x <= limit)){
+                    ans+=1;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
