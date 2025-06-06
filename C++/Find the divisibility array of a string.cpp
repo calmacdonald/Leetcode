8c31a746cb5bcdf1b432323f37bd9d3680204584
cmacdona <cmacdona@eecs.wsu.edu>
2025-06-05 23:12:43 -0700

    Added problem

---
+++ C++/Find the divisibility array of a string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> divisibilityArray(string word, int m) {
+        vector<int> ans(word.size(), 0);
+        long long t = 0;
+        for(int i = 0; i < word.size(); ++i){
+            t = (t * 10 + (word[i] - '0') ) % m;
+            if(t == 0) ans[i] = 1; 
+        }
+        return ans;
+    }
+};
