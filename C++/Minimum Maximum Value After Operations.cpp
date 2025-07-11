5f50fe31679ce8edd57fe3e6cb183aa6cddb5081
Callum Laptop <callumian@live.com>
2025-07-10 21:18:42 -0700

    Added problem

--- C++/Minimum Maximum Value After Operations.cpp
+++ C++/Minimum Maximum Value After Operations.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int minimumMaximumValue(vector<int>& nums) {
         long sum = 0, res = 0;
-        for (int i = 0; i < A.size(); ++i) {
-            sum += A[i];
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
             res = max(res, (sum + i) / (i + 1));
         }
         return res;
