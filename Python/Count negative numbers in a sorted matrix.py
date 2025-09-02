a338297176e3ab8a0c5caebf92257983d65b5acf
Callum Laptop <callumian@live.com>
2025-09-01 23:53:36 -0700

    Added problem

---
+++ Python/Count negative numbers in a sorted matrix.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def countNegatives(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(grid)-1, -1,-1):
+            for j in range(len(grid[0])-1,-1, -1):
+                if grid[i][j]<0:
+                    count +=1
+        return(count)
