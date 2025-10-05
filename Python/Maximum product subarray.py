3b5dd7475f737f22b09a32068006baa168e997fc
Callum Laptop <callumian@live.com>
2025-10-04 23:53:10 -0700

    Added Problem

---
+++ Python/Maximum product subarray.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maxProduct(self, A):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        B = A[::-1]
+        for i in range(1, len(A)):
+            A[i] *= A[i - 1] or 1
+            B[i] *= B[i - 1] or 1
+        return max(A + B)
