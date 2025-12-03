541b293d1d85b21ee80d9ddd3130148c0f7eac04
Callum Laptop <callumian@live.com>
2025-12-02 20:18:53 -0800

    Added problem

---
+++ C++/Removing minimum and maximum from array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        int i = 0, j = 0, n = nums.size();
+        for (int k = 0; k < n; ++k) {
+            if (nums[i] < nums[k]) i = k;
+            if (nums[j] > nums[k]) j = k;
+        }
+        return min({max(i + 1, j + 1), max(n - i, n - j), i + 1 + n - j, j + 1 + n - i});
+    }
+};
