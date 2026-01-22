0826efde8f6892698cb67ed173aca7a950d06f65
Callum Laptop <callumian@live.com>
2026-01-21 23:38:16 -0800

    Added problem

---
+++ C++/Distribute elements into two arrays.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans;
+        vector<int>v1,v2;
+        v1.push_back(nums[0]);
+        v2.push_back(nums[1]);
+        for(int i=2;i<n;i++) {
+            if(v1.back()>v2.back())
+                v1.push_back(nums[i]);
+            else
+                v2.push_back(nums[i]);
+        }
+        ans=v1;
+        for(auto i:v2)
+            ans.push_back(i);
+        return ans;
+    }
+};
