adf3c7397c98711759de4ddbe82070e26a71585f
Callum Laptop <callumian@live.com>
2026-03-18 23:46:00 -0700

    Added problem

---
+++ C++/Distribute candies.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        unordered_map<int, int> mp;
+        for(int i = 0; i < candyType.size(); i++) mp[candyType[i]]++;
+        int n = candyType.size() / 2;
+        return (n < mp.size()) ? n : mp.size();
+    }
+};
